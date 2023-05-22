e41af528b7e4e5a3b8296278c00592ac8c1dd913
hovanduong <duonghv@easysalon.vn>
2023-05-22 10:14:04 +0700

    feat(AN-55): DONE 1

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many ```````timexxxs:',
+              'You have pushed the button this many ```````timexxxsadfasdfdsas:',
             ),
             Text(
               '$_counter',
